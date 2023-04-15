a5061117049dbee55e9fb96f2d957967b5d1eb0f
adr101mg González <adr101mg@hotmail.com>
2023-04-15 15:38:09 -0600

    preparando app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {BrouserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
