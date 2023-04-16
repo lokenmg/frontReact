ee188a407667b04f42ede60a429f008aad6f359d
adr101mg González <adr101mg@hotmail.com>
2023-04-15 19:26:50 -0600

    finalización del proyecto

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import imageRickMorty from './img/rick-morty.png'
+import faceboock from './img/facebook.png'
+import youtube from './img/youtube.png'
+import twitter from './img/gorjeo.png'
 import "./App.css"
 import { useState } from 'react';
 import Characters from './components/characters';
@@ -18,6 +21,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        
+
         <h1 className="title">Rick & morty</h1>
         {characters ? (
           <Characters characters={characters} setCharacters={setCharacters} />
@@ -31,9 +36,23 @@ function App() {
         )
 
         };
-
+      <h1 className="title">By Rodrigo Mencias Gonzalez</h1>
+      <h1 className="title">Zs20006748</h1>
 
       </header>
+
+
+
+      <footer>
+
+
+      <div className="social-icons">
+        
+        <a href="https://twitter.com/rodrigomencias2"><img src={twitter} alt="Twitter"/></a>
+        <a href="https://www.facebook.com/rodrigo.mencias.73/"><img src={faceboock} alt="Facebook" /></a>
+        <a href="https://www.youtube.com/channel/UCOq5scgXfhV-muBmO3G3yKg"><img src={youtube} alt="Youtube" /></a>
+      </div>
+    </footer>
     </div>
     
   );
