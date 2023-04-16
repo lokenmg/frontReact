3ab7fb3f2223dd8a2bacf71a8563307edc31dbed
adr101mg González <adr101mg@hotmail.com>
2023-04-15 18:52:03 -0600

    finalizacion del programa para del proyecto

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
 import imageRickMorty from './img/rick-morty.png'
 import "./App.css"
-import {BrouserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import { useState } from 'react';
+import Characters from './components/characters';
+import { FaTwitter, FaFacebook, FaInstagram } from 'react-icons/fa';
+
+
 function App() {
-  const reqApi =() =>{
-    console.log("Clicking");
+  const [characters, setCharacters] = useState(null);
+
+  const reqApi = async () => {
+    const api = await fetch('https://rickandmortyapi.com/api/character');
+    const characterApi = await api.json();
+    setCharacters(characterApi.results);
   }
+
+
+
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="title">Rick & morty</h1>
-        <img src={imageRickMorty} alt='Rick & MOrty' className='img-home' />
-        <button onClick={reqApi} className='btn-search'>
-          Buscar personajes
-        </button>
+        {characters ? (
+          <Characters characters={characters} setCharacters={setCharacters} />
+        ) : (
+          <>
+            <img src={imageRickMorty} alt='Rick & MOrty' className='img-home' />
+            <button onClick={reqApi} className='btn-search'>
+              Buscar personajes
+            </button>
+          </>
+        )
+
+        };
+
+
       </header>
     </div>
+    
   );
 }
 
